8f9bcd5578755d8ea290c3a7901225984870e3eb
Alec Roques <alec.roques@gmail.com>
2018-11-18 15:33:04 -0600

    Pass in target function as variable

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    p1, p2 = get_experiment_data(num_datasets=10000000)
+    num_datasets = 10000000
+    target_fn = np.square
+    p1, p2 = get_experiment_data(num_datasets, target_fn)
     m = get_slope(p1, p2)
     b = get_y_intercept(p1, m)
 
@@ -10,31 +12,31 @@ def main():
     b_avg = round(np.average(b), 2)
 
     avg_g = 'y = {}x + {}'.format(m_avg, b_avg)
-    bias = calculate_bias(get_x(), m_avg, b_avg)
-    var = calculate_var(get_x(), m, b, m_avg, b_avg)
+    bias = calculate_bias(get_x(num_datasets), m_avg, b_avg, target_fn)
+    var = calculate_var(get_x(num_datasets), m, b, m_avg, b_avg)
     
     print_data('avg g(x)', avg_g)
     print_data('bias', bias)
     print_data('var', var)
-    print_data('eout', round(bias + var, 2))
+    print_data('e_out', round(bias + var, 2))
 
-    plot_exp(m_avg, b_avg)
+    plot_exp(m_avg, b_avg, target_fn)
 
 
 def print_data(label, value, width=10):
     print('{:{}}: {}'.format(label, width, value))
 
 
-def get_experiment_data(num_datasets):
+def get_experiment_data(num_datasets, target_fn):
     num_samples = num_datasets * 2
     s = np.random.uniform(-1, 1, num_samples)
     s = s.reshape(num_datasets, 2)
 
     x1, x2 = s[:, 0], s[:, 1]
-    x1_squared, x2_squared = np.square(x1), np.square(x2)
+    y1, y2 = target_fn(x1), target_fn(x2)
 
-    p1 = np.column_stack((x1, x1_squared))
-    p2 = np.column_stack((x2, x2_squared))
+    p1 = np.column_stack((x1, y1))
+    p2 = np.column_stack((x2, y2))
 
     return p1, p2
 
@@ -52,10 +54,10 @@ def get_y_intercept(p, m):
     return -m * p[:, 0] + p[:, 1]
 
 
-def calculate_bias(x, m_avg, b_avg):
+def calculate_bias(x, m_avg, b_avg, target_fn):
     g_avg = m_avg * x + b_avg
-    f_x = np.square(x)
-    return round(np.average(np.square(g_avg - f_x)), 2)
+    f_x = target_fn(x)
+    return round(np.average(target_fn(g_avg - f_x)), 2)
 
 
 def calculate_var(x, m, b, m_avg, b_avg):
@@ -64,11 +66,11 @@ def calculate_var(x, m, b, m_avg, b_avg):
     return round(np.average(np.square(g_x - g_avg)) / x.shape[0], 2)
 
 
-def get_x():
-    return np.linspace(-1, 1, 10000000)
+def get_x(num_pts):
+    return np.linspace(-1, 1, num_pts)
 
 
-def plot_exp(m_avg, b_avg):
+def plot_exp(m_avg, b_avg, targer_fn):
     plt.style.use('seaborn-whitegrid')
     fig, ax = plt.subplots()
     
@@ -76,7 +78,7 @@ def plot_exp(m_avg, b_avg):
 
     x = np.linspace(-1, 1, 30)
     
-    ax.plot(x, np.square(x), label='f(x)')
+    ax.plot(x, targer_fn(x), label='f(x)')
     ax.plot(x, m_avg * x + b_avg, color='r', label='avg g(x)')
     
     ax.legend(facecolor='w', fancybox=True, frameon=True, edgecolor='black', borderpad=1)
