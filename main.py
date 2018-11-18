8065111fc2d36b770443822c469bdfa5e451c6ae
Alec Roques <alec.roques@gmail.com>
2018-11-18 11:34:33 -0600

    Add slope and y-intercept getters, have experiment data be points

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 def main():
-    x = get_experiment_data(num_samples=20000)
-    print(x[0])
+    p1, p2 = get_experiment_data(num_samples=20000)
+    print(p1[0])
+    print(p2[0])
+
+def get_y_intercept(p, m):
+    # y - y1 = m(x - x1)
+    # y - y1 = mx - mx1
+    # y = mx - mx1 + y1
+    # let b = -mx1 + y1
+    return -m * p[0] + p[1]
+
+
+def get_slope(p1, p2):
+    # slope = (y2 - y1) / (x2 - x1)
+    return p2[1] - p1[1] / p2[0] - p1[0]
 
 
 def get_experiment_data(num_samples):
@@ -11,9 +24,12 @@ def get_experiment_data(num_samples):
     s = s.reshape(num_rows, 2)
 
     x1, x2 = s[:, 0], s[:, 1]
-    x1_squared, x2_squared = np.square(x1), np.square(x2) 
+    x1_squared, x2_squared = np.square(x1), np.square(x2)
 
-    return np.column_stack((x1, x1_squared, x2, x2_squared))
+    p1 = np.column_stack((x1, x1_squared))
+    p2 = np.column_stack((x2, x2_squared))
+
+    return p1, p2
 
 if __name__ == '__main__':
     main()
